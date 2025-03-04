39490ac9c70871dc21fecea83063c481b30a19d7
Avishkar2004 <avishkarkakde2004@gmail.com>
2025-03-04 10:13:51 +0530

    Delete Workflow Done

--- app/(dashboard)/workflows/_components/WorkflowCard.tsx
+++ app/(dashboard)/workflows/_components/WorkflowCard.tsx
@@ -85,14 +85,23 @@ const WorkflowCard = ({ workflow }: { workflow: Workflow }) => {
             <ShuffleIcon size={16} />
             Edit
           </Link>
-          <WorkflowActions workflowName={workflow.name} />
+          <WorkflowActions
+            workflowName={workflow.name}
+            workflowId={workflow.id}
+          />
         </div>
       </CardContent>
     </Card>
   );
 };
 
-function WorkflowActions({ workflowName }: { workflowName: string }) {
+function WorkflowActions({
+  workflowName,
+  workflowId,
+}: {
+  workflowId: string;
+  workflowName: string;
+}) {
   const [showDeleteDialog, setShowDeleteDialog] = useState(false);
   return (
     <>
@@ -100,6 +109,7 @@ function WorkflowActions({ workflowName }: { workflowName: string }) {
         open={showDeleteDialog}
         setOpen={setShowDeleteDialog}
         workflowName={workflowName}
+        workflowId={workflowId}
       />
       <DropdownMenu>
         <DropdownMenuTrigger>
