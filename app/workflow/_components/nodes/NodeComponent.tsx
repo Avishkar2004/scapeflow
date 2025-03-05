598907c043ef1074916aa82053cd6f3b4922f115
Avishkar2004 <avishkarkakde2004@gmail.com>
2025-03-05 22:58:01 +0530

    Copy Node Header

--- app/workflow/_components/nodes/NodeComponent.tsx
+++ app/workflow/_components/nodes/NodeComponent.tsx
@@ -12,13 +12,12 @@ const NodeComponent = memo((props: NodeProps) => {
   const task = TaskRegistry[nodeData.type];
   return (
     <NodeCard nodeId={props.id} isSelected={!!props.selected}>
-      <NodeHeader taskType={nodeData.type} />
+      <NodeHeader taskType={nodeData.type} nodeId={props.id} />
       <NodeInputs>
         {task.inputs.map((input) => (
           <NodeInput key={input.name} input={input} nodeId={props.id} />
         ))}
       </NodeInputs>
-
       <NodeOutputs>
         {task.outputs.map((output) => (
           <NodeOutput key={output.name} output={output} />
